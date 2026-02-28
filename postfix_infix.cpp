ffdb0750ec3b7d6a21eda5a7297a78ba9ee1f190
Jaskiran <jaskiranchahal211@gmail.com>
2026-02-28 19:28:24 +0530

    postfix to infix

--- postfix_infix.cpp
+++ postfix_infix.cpp
@@ -1,36 +1,33 @@
 #include<iostream>
 using namespace std;
-#include<string>
 #include<stack>
-class Solution{
+#include<string>
+class Solution {
 public:
-stack<string>st;
-    string prefixInfix(string s)
-    {
-        for(int i=s.size()-1;i>=0;i--)
+    string postToInfix(string s) {
+        stack<string>st;
+        for(char ch:s)
         {
-            char ch=s[i];
             if(isalnum(ch))
             {
                 st.push(string(1,ch));
             }
             else{
-                string operator1=st.top();st.pop();
-                string operator2=st.top();st.pop();
-                string temp="("+operator1+string(1,ch)+operator2+")";
+                string op2=st.top();st.pop();
+                string op1=st.top();st.pop();
+                string temp='('+op1+string(1,ch)+op2+')';
                 st.push(temp);
             }
         }
-return st.top();
-
+        return st.top();
+        
     }
-
 };
 int main()
 {
-    string m;
-    cout<<"enter string="<<endl;
-    cin>>m;
     Solution s;
-    s.prefixInfix(m);
+    string m;
+    cout<<"enter expression"<<endl;
+    cin>>m;
+    s.postToInfix(m);
 }
