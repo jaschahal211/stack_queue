2a0aa81cee768ccea373590f845942f3472523c2
Jaskiran <jaskiranchahal211@gmail.com>
2026-02-28 14:46:01 +0530

    minstack

---
+++ minstack.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class MinStack {
+private:
+    stack<int> s;
+    stack<int> mins;
+
+public:
+    MinStack() {
+    }
+    
+    void push(int val) {
+        s.push(val);
+
+        if (mins.empty() || val <= mins.top()) {
+            mins.push(val);
+        }
+    }
+    
+    void pop() {
+        if (s.top() == mins.top()) {
+            mins.pop();
+        }
+        s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return mins.top();
+    }
+};
