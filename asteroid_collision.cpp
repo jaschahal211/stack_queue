d909dac3aa44400e3e04f423d67e4327d1434e92
Jaskiran <jaskiranchahal211@gmail.com>
2026-03-01 20:27:10 +0530

    asteroid collision

---
+++ asteroid_collision.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#include<stack>
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st;   // using vector as stack
+        
+        for (int a : asteroids) {
+            
+            bool destroyed = false;
+            
+            while (!st.empty() && st.back() > 0 && a < 0) {
+                
+                if (abs(st.back()) < abs(a)) {
+                    st.pop_back();      
+                    continue;           
+                }
+                else if (abs(st.back()) == abs(a)) {
+                    st.pop_back();      
+                }
+                destroyed = true;
+                break;
+            }
+            
+            if (!destroyed) {
+                st.push_back(a);
+            }
+        }
+        
+        return st;
+    }
+};
