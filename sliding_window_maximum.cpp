2f4310c61a5525a06c01395931937d3e049e7b7e
Jaskiran <jaskiranchahal211@gmail.com>
2026-03-05 11:03:31 +0530

    finding maximum in sliding window using deque

---
+++ sliding_window_maximum.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int>dq;
+        vector<int>ans;
+        for(int i=0;i<k;i++)
+        {
+            while(!dq.empty()&&nums[dq.back()]<=nums[i])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        ans.push_back(nums[dq.front()]);
+        for(int i=k;i<nums.size();i++)
+        {
+            while(!dq.empty()&&dq.front()<i-k)
+            {
+                dq.pop_front();
+            }
+             while(!dq.empty()&&nums[dq.back()]<=nums[i])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            ans.push_back(nums[dq.front()]);
+
+        }
+        return ans;
+    }
+};
+ 
